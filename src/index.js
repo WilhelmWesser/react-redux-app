2142558df548310698cea03e92cf241f3e619a53
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-01-24 11:19:38 +0200

    Task 17 part 2 is done

--- src/index.js
+++ src/index.js
@@ -1,38 +1,56 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
-import * as actions from "./store/actions";
-import { initiateStore } from "./store/store";
+import {
+  titleChanged,
+  taskDeleted,
+  completeTask,
+  loadTasks,
+  getTasks,
+  createTask,
+  getTasksLoadingStatus,
+} from "./store/task";
+import configureStore from "./store/store";
+import { Provider } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { getError } from "./store/errors";
 
-const store = initiateStore();
+const store = configureStore();
 
 const App = (params) => {
-  const [state, setState] = useState(store.getState());
+  const state = useSelector(getTasks());
+  const isLoading = useSelector(getTasksLoadingStatus());
+  const error = useSelector(getError());
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    store.subscribe(() => {
-      setState(store.getState());
-    });
+    dispatch(loadTasks());
   }, []);
 
-  const completeTask = (taskId) => {
-    store.dispatch(actions.taskCompleted(taskId));
-  };
   const changeTitle = (taskId) => {
-    store.dispatch(actions.titleChanged(taskId));
+    dispatch(titleChanged(taskId));
   };
   const deleteTask = (taskId) => {
-    store.dispatch(actions.taskDeleted(taskId));
+    dispatch(taskDeleted(taskId));
   };
+  if (isLoading) {
+    return <h1>Loading</h1>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   return (
     <>
       <h1> App</h1>
+      <button onClick={() => dispatch(createTask())}>Add new task</button>
       <ul>
         {state.map((el) => (
           <li key={el.id}>
             <p>{el.title}</p>
             <p> {`Completed: ${el.completed}`}</p>
-            <button onClick={() => completeTask(el.id)}>Complete</button>
+            <button onClick={() => dispatch(completeTask(el.id))}>
+              Complete
+            </button>
             <button onClick={() => changeTitle(el.id)}>Change title</button>
             <button onClick={() => deleteTask(el.id)}>Delete</button>
             <hr />
@@ -45,7 +63,9 @@ const App = (params) => {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/services/todos.service.js
@@ -0,0 +1,22 @@
+import httpService from "./http.service";
+const todosEndpoint = "todos/";
+const todosService = {
+  fetch: async () => {
+    const { data } = await httpService.get(todosEndpoint, {
+      params: {
+        _page: 1,
+        _limit: 10,
+      },
+    });
+    return data;
+  },
+  getNewTask: async () => {
+    const { data } = await httpService.post(todosEndpoint, {
+      title: "NEW FROM JSONPLACEHOLDER",
+      completed: false,
+    });
+    console.log(data);
+    return data;
+  },
+};
+export default todosService;
